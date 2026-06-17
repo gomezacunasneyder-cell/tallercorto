fe6a9278099ff811cd266a839d231e62014e42d6
stevenzz <gomezacunasneyder@gmail.com>
2026-06-16 19:11:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,21 @@
-
-const persona = {
-    nombre: "Juan",
-    edad: 23,
-    altura: 1.68,
-    peso: "60 kg"
-};
-console.log(persona);
-
-
 const persona = {
     nombre: "Juan",
     edad: 23,
     altura: 1.68,
     peso: "60 kg",
-    
 
     caminar() {
         console.log("Estoy caminando");
     }
 };
 
+console.log(persona);
 
 persona.caminar();
 
-
 const personaJSON = JSON.stringify(persona);
 
-
 console.log(personaJSON);
-
-
 
 const listaEmpleados = [
     { nombre: "Juan Pérez", apellido: "López" },
@@ -37,27 +23,17 @@ const listaEmpleados = [
     { nombre: "Pedro", apellido: "Sánchez" }
 ];
 
-
 console.log(listaEmpleados);
-
-
 
 const frutas = ["Manzana", "Banana", "Naranja"];
 
-
-
 const posicionManzana = frutas.indexOf("Manzana");
-
 
 console.log(posicionManzana);
 
 const edades = [25, 30, 18, 15];
 
-
-
 const primerMenor = edades.find(edad => edad < 20);
 
-
 console.log(primerMenor);
-
 
